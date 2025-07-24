48920f801f2742a9ee5aae6edc4cafce312c9d89
Hershraj Niranjani <hershraj.niranjani@propertyfinder.ae>
2025-07-25 04:36:13 +0530

    Change a test to be possible now
    After some code changes, this test was impossible to pass. Changed it to match the new refactor.

--- tests/test_utilities/test_config.py
+++ tests/test_utilities/test_config.py
@@ -1,15 +1,9 @@
 """Unit tests for flowzero_src.util.config helper module."""
 
-from flowzero_src.util.config import get_key, is_verbose
+from flowzero_src.util.config import get_key
 
 
 def test_get_key():
-    assert get_key("logging.verbose") is True or get_key("logging.verbose") is False, (
-        "Expected to successfully read 'logging.verbose' to be a boolean value."
-    )
-
-
-def test_is_verbose():
-    assert is_verbose() is True or is_verbose() is False, (
-        "Expected 'is_verbose' to return a boolean value."
+    assert get_key("mcgs.sims_per_move") > 0, (
+        "Expected to successfully read 'mcgs.sims_per_move' to be a positive integer."
     )
